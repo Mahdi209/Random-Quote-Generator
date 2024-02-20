b196bf7162dc208170c6d5a7208b63678b9969f3
Mahdi209 <dr.arcadea@gmail.com>
2024-02-20 19:00:25 +0300

    task is done

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
+//declare
 const quoteList = document.getElementById("quote-list");
 const newQuoteButton = document.getElementById("new-quote-button");
 
+//action
 newQuoteButton.addEventListener("click", () => {
+  //array
   const quotes = ["hello!", "why!", "where!", "when!", "ok!"];
 
+  //random index
   const quoteIndex = Math.floor(Math.random() * quotes.length);
 
+  //chocs random index
   const newQuote = quotes[quoteIndex];
 
+  //create element
   const quoteContainer = document.createElement("div");
 
+  //give id name
   quoteContainer.id = "quote-container";
 
+  //append to container
   quoteList.appendChild(quoteContainer);
 
+  //create element
   const quotePar = document.createElement("p");
 
+  //give id name
   quotePar.id = "quote-text";
 
+  //add quote to container
   quotePar.innerHTML = newQuote;
 
+  //append to container
   quoteContainer.appendChild(quotePar);
-});
+}); //end for action
